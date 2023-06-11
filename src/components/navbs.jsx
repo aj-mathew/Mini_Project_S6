97117a17926d0f55ac2b5d575b1fda7be935f15f
aj.mathew <alvinjohnmathew@gmail.com>
2023-06-11 13:45:05 +0530

    parking spots, booking form and confirmation

--- src/components/navbs.jsx
+++ src/components/navbs.jsx
@@ -7,7 +7,7 @@ function Navbs() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Parkify</Navbar.Brand>
+        <Navbar.Brand href="/">Parkify</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
