b85d846020c7ad97274b8e00c5e3e28bb6205fd9
MathewAbraham8 <mathewabraham5010@gmail.com>
2023-06-16 00:06:48 +0530

    testu

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
-var express = require('express')
-var app=express()
-app.use(express.json())
-var cors=require("cors")
-app.use(cors())
+const express=require("express")
+const mongoose=require("mongoose")
+const cors=require("cors")
+
+var app= express()
 app.use(express.urlencoded({extended:true}))
-const mongoose = require('mongoose');
+app.use(cors())
+app.use(express.json())
 
-
+//mongoose code
+app.listen(9000,()=>{
+    console.log("server running http://localhost:9000/")
+})
 
 main().catch(err => console.log(err));
 
@@ -15,20 +19,30 @@ async function main() {
   console.log("Connected and Running.....")
 // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+//schema create
 const formSchema =new mongoose.Schema({
+    fullname : String,
+    lastname : String,
     email : String,
     password :String
-})
-const Empinfo =mongoose.model('Empinfo',formSchema)
+});
+const formmodel =mongoose.model('user_tbl',formSchema)
+//api for register
 app.post('/reg',(req,res)=>{
-    var email=req.body.eml
-    var password=req.body.pss
-    res.send({'msg':'success'})
+    var fname=req.body.fname;
+    var lname=req.body.lname;
+    var eml=req.body.eml;
+    var passw=req.body.passw;
+    const table= new formmodel({fullname:fname,lastname:lname,email:eml,password:passw})
+    table.save().then(()=>{
+        res.send({"msg":"Registration successful"})
+    })
+    {/*res.send({'msg':'success'})
     console.log(email+" "+password)
-    
+
     const emp= new Empinfo({email:email,password:password})
     emp.save() //should we empty the variable everytime we enter the value
-    
+*/}
 })
 const stuSchema=new mongoose.Schema({
     name:String,
@@ -38,17 +52,18 @@ const stuSchema=new mongoose.Schema({
     course:String
 
 })
-const Stuinfo = mongoose.model('Stuinfo',stuSchema)
+const ownermodel = mongoose.model('owner_tbl',formSchema)
 app.post('/stureg',(req,res)=>{
-    var name=req.body.nam
-    var email=req.body.eml
-    var phone=req.body.phn
-    var gender=req.body.gen
-    var course=req.body.cose
-    res.send({'msg':'success'})
-    console.log(name+'\n'+email+'\n'+phone+'\n'+gender+course)
-    const stu=new Stuinfo({name:name,email:email,phone:phone,gender:gender+"",course:course})
-    stu.save()
+    var fname=req.body.fname;
+    var lname=req.body.lname;
+    var eml=req.body.eml;
+    var passw=req.body.passw;
+   {/*} res.send({'msg':'success'})
+console.log(name+'\n'+email+'\n'+phone+'\n'+gender+course)*/}
+    const own=new ownermodel({fullname:fname,lastname:lname,email:eml,password:passw})
+    own.save().then(()=>{
+        res.send({"msg":"Registration successful"})
+    })
 })
 app.listen(9000,()=>{
     console.log("server running http://localhost:9000/")
